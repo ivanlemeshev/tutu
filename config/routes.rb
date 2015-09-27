4056af7fbde10baa317fa8129c46a58b0e962066
Ivan Lemeshev <vanlmshv@gmail.com>
2015-09-27 21:34:08 +0600

    add resource routes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :trains
   resources :railway_stations
+  resources :routes
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
