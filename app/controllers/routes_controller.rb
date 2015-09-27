f70f5325b7151c4bcdad669aee75a5e9946c6f4b
Ivan Lemeshev <vanlmshv@gmail.com>
2015-09-27 21:52:38 +0600

    add action create to create new routes

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -3,5 +3,14 @@ class RoutesController < ApplicationController
   end
 
   def create
+    @route = Route.new(route_params)
+    @route.save
+    redirect_to @route
+  end
+
+  private
+
+  def route_params
+    params.require(:route).permit(:title)
   end
 end
