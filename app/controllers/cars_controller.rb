1bc4eed1b0c02161e419ef7c5c5d7c4ffbc6911c
Ivan Lemeshev <vanlmshv@gmail.com>
2015-10-05 04:23:57 +0600

    create cars controller

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,50 @@
+class CarsController < ApplicationController
+  before_action :set_car, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cars = Car.all
+  end
+
+  def show
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def edit
+  end
+
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      redirect_to @car, notice: 'Car was successfully created.'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @car.update(car_params)
+      redirect_to @car, notice: 'Car was successfully updated.'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @car.destroy
+    redirect_to cars_path, notice: 'Car was successfully destroyed.'
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:type, :up_place_count, :down_place_count, :train_id)
+  end
+end
