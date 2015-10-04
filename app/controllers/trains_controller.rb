83db8b19ef957a2181996ce93f5630c9f56f8b94
Ivan Lemeshev <vanlmshv@gmail.com>
2015-10-05 03:59:33 +0600

    add route to the train

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -45,6 +45,6 @@ class TrainsController < ApplicationController
   end
 
   def train_params
-    params.require(:train).permit(:number)
+    params.require(:train).permit(:number, :route_id)
   end
 end
