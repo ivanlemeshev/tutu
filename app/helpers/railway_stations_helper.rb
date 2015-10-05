fed4d79520cd77bdd3afef46875bd9a65d6d38b7
Ivan Lemeshev <vanlmshv@gmail.com>
2015-10-05 06:24:46 +0600

    show information abut train cars

--- app/helpers/railway_stations_helper.rb
+++
@@ -1,2 +0,0 @@
-module RailwayStationsHelper
-end
--- app/models/train.rb
+++ app/models/train.rb
@@ -2,6 +2,18 @@ class Train < ActiveRecord::Base
   validates :number, presence: true, uniqueness: true
 
   belongs_to :route
+
   has_many :tickets
   has_many :cars
+
+  has_many :compartment_cars, -> { compartment }, class_name: 'Car', foreign_key: 'train_id'
+  has_many :economy_cars, -> { economy }, class_name: 'Car', foreign_key: 'train_id'
+
+  def compartment_place_count(type)
+    compartment_cars.sum("#{type}_place_count")
+  end
+
+  def economy_place_count(type)
+    economy_cars.sum("#{type}_place_count")
+  end
 end
