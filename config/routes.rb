c3af0c3744a7456a7f40f5bc5224e91b735a4863
Ivan Lemeshev <vanlmshv@gmail.com>
2015-10-06 05:34:47 +0600

    add tickets resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :cars
   resources :railway_stations
   resources :routes
+  resources :tickets
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
