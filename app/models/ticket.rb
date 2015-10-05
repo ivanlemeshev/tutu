1a41384eabc5911db4e7d5e1ac6f759677532ce8
Ivan Lemeshev <vanlmshv@gmail.com>
2015-10-06 05:35:16 +0600

    fix ticket model and add validations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,9 @@
 class Ticket < ActiveRecord::Base
+  validates :first_name, :last_name, presence: true
+  validates :train_id, :start_station_id, :end_station_id, presence: true
+
   belongs_to :user
   belongs_to :train
-  belongs_to :start_station, class_name: 'RailwayStation', foreign_key: start_station_id
-  belongs_to :end_station, class_name: 'RailwayStation', foreign_key: end_station_id
+  belongs_to :start_station, class_name: 'RailwayStation', foreign_key: 'start_station_id'
+  belongs_to :end_station, class_name: 'RailwayStation', foreign_key: 'end_station_id'
 end
