e7669a4a58fe829cb5d114b508f94519d0024c68
Ivan Lemeshev <vanlmshv@gmail.com>
2015-10-26 20:00:49 +0600

    add admin flag to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026130744) do
+ActiveRecord::Schema.define(version: 20151026135834) do
 
   create_table "cars", force: :cascade do |t|
     t.integer  "top_seats"
@@ -71,10 +71,10 @@ ActiveRecord::Schema.define(version: 20151026130744) do
     t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20151026130744) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
