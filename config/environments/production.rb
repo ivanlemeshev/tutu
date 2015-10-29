3c63f4570953a9d9b4095fc7cbb316fa56d95044
Ivan Lemeshev <vanlmshv@gmail.com>
2015-10-29 21:15:27 +0600

    remove mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'shielded-mountain-2166.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'shielded-mountain-2166.herokuapp.com',
-    :authentication => :plain,
-  }
 end
