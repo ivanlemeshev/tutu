88b3a2bfe5a342d7d5bff63bd738424a0f0ad7d1
Ivan Lemeshev <vanlmshv@gmail.com>
2015-10-29 21:04:57 +0600

    add heroku host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.default_url_options = { :host => 'shielded-mountain-2166.herokuapp.com' }
 end
